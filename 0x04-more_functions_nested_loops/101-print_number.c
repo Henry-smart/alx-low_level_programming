70914b01890472fba3d8069e13444ed954c563e4
henrysmart <henryeziechina2@gmail.com>
2022-10-21 03:42:04 +0100

    refix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,27 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_number - that prints an integer
- * @n: print int
- * Return: Always 0.
+ * print_number - Prints an integer.
+ * @n: The integer to print.
+ * Return: void
  */
+
 void print_number(int n)
 {
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	return (0);
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+	n *= -1;
+	k = n;
+	_putchar('-');
+	}
+
+	k /= 10;
+
+	if (k != 0)
+	print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
 }
